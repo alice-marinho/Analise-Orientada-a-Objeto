0719f30767b84b5639af93b0afe079d3ac359c43
Alice <133613280+alicegms@users.noreply.github.com>
2023-08-12 14:51:51 -0300

    Aula3 Exemplo

---
+++ Aula_3/Aula_3/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Aula_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.CalculaMedia = new System.Windows.Forms.Button();
+            this.labelVeja = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(254, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(254, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nota 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(257, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nota 2";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(302, 99);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 3;
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.Location = new System.Drawing.Point(299, 145);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(100, 20);
+            this.txtNota1.TabIndex = 4;
+            this.txtNota1.TextChanged += new System.EventHandler(this.txtNota1_TextChanged);
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.Location = new System.Drawing.Point(302, 186);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(100, 20);
+            this.txtNota2.TabIndex = 5;
+            // 
+            // CalculaMedia
+            // 
+            this.CalculaMedia.Location = new System.Drawing.Point(323, 233);
+            this.CalculaMedia.Name = "CalculaMedia";
+            this.CalculaMedia.Size = new System.Drawing.Size(75, 23);
+            this.CalculaMedia.TabIndex = 6;
+            this.CalculaMedia.Text = "Calcular Media";
+            this.CalculaMedia.UseVisualStyleBackColor = true;
+            this.CalculaMedia.Click += new System.EventHandler(this.CalculaMedia_Click);
+            // 
+            // labelVeja
+            // 
+            this.labelVeja.AutoSize = true;
+            this.labelVeja.Location = new System.Drawing.Point(256, 275);
+            this.labelVeja.Name = "labelVeja";
+            this.labelVeja.Size = new System.Drawing.Size(31, 13);
+            this.labelVeja.TabIndex = 7;
+            this.labelVeja.Text = "Veja:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelVeja);
+            this.Controls.Add(this.CalculaMedia);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtNota1);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "txtMostra";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.Button CalculaMedia;
+        private System.Windows.Forms.Label labelVeja;
+    }
+}
+
---
+++ Aula_3/Aula_3/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula_3
+{
+    public partial class Form1 : Form
+    {
+        Aluno aluno1 = new Aluno();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculaMedia_Click(object sender, EventArgs e)
+        {
+            aluno1.Nome = txtNome.Text;
+            aluno1.Nota1 = Convert.ToDouble(txtNota1.Text);
+            aluno1.Nota2 = Convert.ToDouble(txtNota2.Text);
+            labelVeja.Text = aluno1.CalcularMedia();
+        }
+
+        private void txtNota1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
